2bc05765059015e9182dc493d822ed23685a5dea
JazzticeGit <asadtevy94@gmail.com>
2025-10-04 19:04:56 +0700

    commit

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
     <title>Document</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
 </head>
 <body>
@@ -49,8 +52,29 @@
     <!-- =============================== MAIN CONTENT ============================== -->
 
     <div class="container">
-        <h1>Selamat <span>Datang di</span> Pulau Dewata</h1>
+        <p class="txt-1">Selamat <br>Datang di <br> <span>Pulau Dewata</span> </p>
+
+        <img src="image/download (5).jpeg" alt="" srcset="" class="img1">
         
+        <p class="txt-2">Jelajahi pesona Pulau Dewata dengan pengalaman tak terlupakan.Dari pantai berpasir putih, budaya yang kaya, hingga panorama alam yang memukau, semua bisa kamu temukan di sini</p>
+
+        <a href="wisata.php" target="_blank" rel="noopener noreferrer"><button>Semua Wisata</button></a>
+
+
+        <!-- ===== Sebelah Kanan ======= -->
+
+
+        <div class="right-main">
+            <img src="image/b3580b1961a4f7404a1e9a5a1b8ee123.jpg" alt="" srcset="">
+
+            <div class="glass-fx">
+                <p>Taman budaya</p>
+                <h2>GWK</h2>
+                <p>(Garuda Wisnu Kencana)</p>
+
+                <a href="wisata.php?id"><button>Lihat Wisata</button></a>
+            </div>
+        </div>
     </div>
 </body>
 </html>
