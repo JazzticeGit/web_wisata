5a4c2fdbd958410becde70941190619004d678d4
JazzticeGit <asadtevy94@gmail.com>
2025-10-04 14:40:44 +0700

    commit

--- index.php
+++ index.php
@@ -4,9 +4,45 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
 </head>
 <body>
-    
-    
+
+    <!-- ===================================== NAV ========================================== -->
+    <!-- ===================================== NAV ========================================== -->
+
+    <nav>
+        <!-- logo -->
+        <div class="logo">
+            <img src="" alt="" srcset="">
+        </div>
+
+<!-- navbar -->
+    <div class="navbbar">
+            <ul>
+                <li class="li1"><a href=".php">Home</a></li>
+                <li class="li2"><a href=".php">Tentang</a></li>
+                <li class="li3"><a href=".php">Wisata</a></li>
+            </ul>
+    </div>
+
+    <!-- icon link -->
+        <div class="iconlink">
+            <ul>
+                <li><i class="fa-solid fa-bookmark"></i></li>
+                <li><i class="fa-solid fa-user"></i></li>
+            </ul>
+        </div>
+    </nav>
+
+    <!-- ============================== SEARCH BAR ================================= -->
+    <!-- ============================== SEARCH BAR ================================= -->
+
+    <form action="search.php" method="post" class="search">
+        <input type="text" name="search" id="search" placeholder="Search">
+        <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+    </form>
 </body>
 </html>
